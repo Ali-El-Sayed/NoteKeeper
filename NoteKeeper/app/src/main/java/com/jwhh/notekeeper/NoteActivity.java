edda85da277a180186c78e55b9b32fb1f1411f06
Ali El-Sayed <aliloka848@gmail.com>
2022-08-02 22:59:44 +0200

    Refactor the actionBar item next and fix the out of bound bug.

--- NoteKeeper/app/src/main/java/com/jwhh/notekeeper/NoteActivity.java
+++ NoteKeeper/app/src/main/java/com/jwhh/notekeeper/NoteActivity.java
@@ -135,6 +135,14 @@ public class NoteActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public boolean onPrepareOptionsMenu(@NonNull Menu menu) {
+        MenuItem item = menu.findItem(R.id.action_next);
+        int lastNoteIndex = DataManager.getInstance().getNotes().size() - 1;
+        item.setEnabled(mNotePosition < lastNoteIndex);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
     private void moveNext() {
         saveNote();
         ++mNotePosition;
@@ -142,6 +150,7 @@ public class NoteActivity extends AppCompatActivity {
 
 //        saveOriginalNoteValues();
         displayNote(mSpinnerCourses, mTextNoteTitle, mTextNoteText);
+        invalidateMenu();
     }
 
     @Override
