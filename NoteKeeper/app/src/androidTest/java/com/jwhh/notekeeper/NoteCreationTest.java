e25122e4e72b02e1d339cc699fe817c5266da46c
Ali El-Sayed <aliloka848@gmail.com>
2022-08-22 15:15:17 +0200

    add spinner test

--- NoteKeeper/app/src/androidTest/java/com/jwhh/notekeeper/NoteCreationTest.java
+++ NoteKeeper/app/src/androidTest/java/com/jwhh/notekeeper/NoteCreationTest.java
@@ -1,27 +1,37 @@
 package com.jwhh.notekeeper;
 
+
 import androidx.test.espresso.Espresso;
-import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.action.ViewActions.*;
-import androidx.test.espresso.action.ViewActions.*;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
 import junit.framework.TestCase;
 
 import org.hamcrest.Matcher;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
 import static androidx.test.espresso.Espresso.*;
 import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static org.hamcrest.Matcher.*;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
 
 @RunWith(AndroidJUnit4.class)
 public class NoteCreationTest extends TestCase {
+
+    static DataManager sDataManager;
+
+    @BeforeClass
+    public static void classSetup() {
+        sDataManager = DataManager.getInstance();
+    }
 
     @Rule
     public ActivityTestRule<NoteListActivity> mNoteListActivityActivityTestRule =
@@ -29,12 +39,22 @@ public class NoteCreationTest extends TestCase {
 
     @Test
     public void createNewNote() {
+        final CourseInfo course = sDataManager.getCourse("java_lang");
+        final String noteTitle = "Test New Note.";
+        final String noteText = "this is the body of our test method";
+
 //        ViewInteraction fabNewNote = onView(withId(R.id.fab));
 //        fabNewNote.perform(click());
+
         onView(withId(R.id.fab)).perform(click());
-        onView(withId(R.id.text_note_title)).perform(typeText("Test New Note."));
+
+        onView(withId(R.id.spinner_courses)).perform(click());
+        onData(allOf(instanceOf(CourseInfo.class), equalTo(course))).perform(click());
+
+        onView(withId(R.id.text_note_title)).perform(typeText(noteTitle));
         onView(withId(R.id.text_note_text)).perform(
-                typeText("this is the body of our test method"),
+                typeText(noteText),
                 ViewActions.closeSoftKeyboard());
+        Espresso.pressBack();
     }
 }
